7b5193aaf2b9e66e7af3cf1f2a0572d1e5838008
Pedro Arthur <panrc100@gmail.com>
2025-05-06 17:34:05 -0300

    Códigos das questões

--- SOR_Sassenfeld.m
+++ SOR_Sassenfeld.m
@@ -1,29 +1,13 @@
-% Método de Gauss-Seidel com verificação de critérios de convergência
+% Método de SOR (Successive Over-Relaxation)
 
-% Definição dos dados do problema
-A = [2 -1 0 0 ;-1 2 -1 0;0 -1 2 -1; 0 0 -1 2];
-b = [1 2 9 11];
-x0 = [0; 0; 0; 0];
-tol = 0.0005;
-fprintf("Utilizando sistema 4x4 (Gauss-Seidel)\n");
+% Dados do problema
+A = [10 3 -2; 2 8 -1; 1 1 5];
+b = [57; 20; -4];
+x0 = [0; 0; 0];
+tol = 0.05;
+omega = 1.2;  % Fator de relaxação
 
-% =========================
-% Critério da diagonal dominante
-% =========================
-dominante = true;
-for i = 1:size(A,1)
-    soma = sum(abs(A(i,:))) - abs(A(i,i));
-    if abs(A(i,i)) <= soma
-        dominante = false;
-        break;
-    end
-end
-
-if dominante
-    fprintf("A matriz é diagonal estritamente dominante por linhas.\n");
-else
-    fprintf("A matriz NÃO é diagonal estritamente dominante por linhas.\n");
-end
+fprintf("Utilizando matriz da questão 1 (SOR - ω = %.2f)\n", omega);
 
 % =========================
 % Critério de Sassenfeld
@@ -55,23 +39,25 @@ else
 end
 
 % =========================
-% Método de Gauss-Seidel
+% Método de SOR
 % =========================
 x = x0;
 err = tol + 1;
 itr = 0;
 
-fprintf("\nIteração\t   x(1)\t\t   x(2)\t\t   x(3)\t\t   x(4)\t\tErro relativo\n");
+fprintf("\nIteração\t   x(1)\t\t   x(2)\t\t   x(3)\t\tErro relativo\n");
 while err > tol
     x_ant = x;
-    x(1) = (b(1) - A(1,2)*x_ant(2) - A(1,3)*x_ant(3) - A(1,4)*x_ant(4)) / A(1,1);
-    x(2) = (b(2) - A(2,1)*x(1) - A(2,3)*x_ant(3) - A(2,4)*x_ant(4)) / A(2,2);
-    x(3) = (b(3) - A(3,1)*x(1) - A(3,2)*x(2) - A(3,4)*x_ant(4)) / A(3,3);
-    x(4) = (b(4) - A(4,1)*x(1) - A(4,2)*x(2) - A(4,3)*x(3)) / A(4,4);
+
+    % Atualizações com fator de relaxação ω
+    x(1) = (1 - omega)*x_ant(1) + (omega/A(1,1))*(b(1) - A(1,2)*x_ant(2) - A(1,3)*x_ant(3));
+    x(2) = (1 - omega)*x_ant(2) + (omega/A(2,2))*(b(2) - A(2,1)*x(1) - A(2,3)*x_ant(3));
+    x(3) = (1 - omega)*x_ant(3) + (omega/A(3,3))*(b(3) - A(3,1)*x(1) - A(3,2)*x(2));
 
     itr = itr + 1;
     err = max(abs((x - x_ant) ./ x));
-    fprintf("%5d\t\t%8.5f\t%8.5f\t%8.5f\t%8.5f\t%e\n", itr, x(1), x(2), x(3), x(4), err);
+
+    fprintf("%5d\t\t%8.5f\t%8.5f\t%8.5f\t%e\n", itr, x(1), x(2), x(3), err);
 
     if itr > 1000
         fprintf("O método não converge para a tolerância especificada.\n");
